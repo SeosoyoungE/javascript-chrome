60915ecdbfa3dd37a51c91d88dbe078db140967d
soyoung <soyseo99@naver.com>
2023-08-25 04:01:01 +0900

    js 변수 선언 방법

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi")
+const a=5;
+const b=2;
+let myName="soyoung";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello "+myName)
+
+myName="soyoungyee";
+
+console.log("my new name is "+myName);
