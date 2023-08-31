650e646d23bee644984189a1f96e4c609cb280fb
soyoung <soyseo99@naver.com>
2023-08-31 17:27:31 +0900

    2.10 - 객체, 함수로 계산기

---
+++ app.js
@@ -0,0 +1,14 @@
+const calculator={
+    plus: function(a,b){
+        console.log(a+b)
+    },
+    min: function(a,b){
+        console.log(a-b)
+    },
+    div: function(a,b){
+        console.log(a/b)
+    },
+    powerof: function(a){
+        console.log(a*a)
+    }
+}
