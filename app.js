b9bcb2aba540b797d2810e9c97cc5e580cdaa508
soyoung <soyseo99@naver.com>
2023-09-01 21:28:59 +0900

    2.14 - isNaN NaN인지 아닌지 판별하는 함수

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let age = parseInt(prompt("How old are you?"));
+//parseInt에 숫자만 쓰여지지 않은 string값을 넣으면 NaN을 출력!
+//하지만 NaN을 typeof로 확인하면 number가 나옴
 
+const age = parseInt(prompt("How old are you?"));
 
-
-console.log(age);
+if(isNaN(age)){
+    console.log(age)
+    console.log("please write a number.")
+} else{
+    console.log("Thank you for writing your age.")
+}
