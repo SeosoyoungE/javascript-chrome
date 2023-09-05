e95ced8ae288315e6fe1d9ec6a9d2a538a49c6a5
soyoung <soyseo99@naver.com>
2023-09-05 19:37:03 +0900

    3.4 - 더 많은 이벤트 종류

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const title=document.querySelector(".hello:first-child h1");
 
+console.dir(title)
+
 function handleTitleClick() {
     title.style.color="green";
 }
 
 title.addEventListener("click", handleTitleClick)
+title.addEventListener("mouseenter", function(){
+    title.innerText="Mouse is here!"
+})
+title.addEventListener("mouseleave", function(){
+    title.innerText="Mouse is gone!"
+})
