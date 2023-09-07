9e550bc298bf75748b73739fd430c2bcc01c05d6
soyoung <soyseo99@naver.com>
2023-09-08 04:53:39 +0900

    3.5 - 이벤트리스너 사용

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const title=document.querySelector(".hello:first-child h1");
+const h1=document.querySelector(".hello:first-child h1");
 
-console.dir(title)
+console.dir(h1)
 
-function handleTitleClick() {
-    title.style.color="green";
+function handleh1Click() {
+    h1.style.color="green";
 }
 
-title.onclick=handleTitleClick;
-title.addEventListener("mouseenter", function(){
-    title.innerText="Mouse is here!"
+h1.onclick=handleh1Click;
+h1.addEventListener("mouseenter", function(){
+    h1.innerText="Mouse is here!"
 })
-title.addEventListener("mouseleave", function(){
-    title.innerText="Mouse is gone!"
+h1.addEventListener("mouseleave", function(){
+    h1.innerText="Mouse is gone!"
+})
+
+window.addEventListener("resize", function(){
+    document.body.style.backgroundColor="tomato";
+})
+window.addEventListener("copy",function(){
+    alert("copy!!!");
+})
+window.addEventListener("offline",function(){
+    alert("SOS no WIFI");
+})
+window.addEventListener("online",function(){
+    alert("all good")
 })
