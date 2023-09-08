40a8b6dacb9ddd95133cfee2ffde679f644c408c
soyoung <soyseo99@naver.com>
2023-09-08 18:30:03 +0900

    3.6 - 함수에서 if-else를 이용하여 구성하기

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 const h1=document.querySelector(".hello:first-child h1");
 
-console.dir(h1)
+
 
 function handleh1Click() {
-    h1.style.color="green";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor!=="blue"){
+        newColor="blue";
+    } else{
+        newColor="tomato";
+    }
+    h1.style.color = newColor;
 }
 
-h1.onclick=handleh1Click;
-h1.addEventListener("mouseenter", function(){
-    h1.innerText="Mouse is here!"
-})
-h1.addEventListener("mouseleave", function(){
-    h1.innerText="Mouse is gone!"
-})
-
-window.addEventListener("resize", function(){
-    document.body.style.backgroundColor="tomato";
-})
-window.addEventListener("copy",function(){
-    alert("copy!!!");
-})
-window.addEventListener("offline",function(){
-    alert("SOS no WIFI");
-})
-window.addEventListener("online",function(){
-    alert("all good")
-})
+h1.addEventListener("click", handleh1Click);
