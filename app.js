be5d7e5bbe02cbef5d8a8e4af4f6cac1f6e02fd5
soyoung <soyseo99@naver.com>
2023-09-08 19:06:50 +0900

    3.8 - classList(기존의 클래스명을 남겨두기위함)의사용 및 토글함수

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const h1=document.querySelector(".hello:first-child h1");
 
 function handleh1Click() {
-    const clickedClass = "clicked"
-    if(h1.className === clickedClass){
-        h1.className = "";
-    } else{
-        h1.className = clickedClass
-    }
+    h1.classList.toggle("clicked")
+    // 토글함수로 아래내용 한줄로
+    // const clickedClass = "clicked"
+    // if(h1.classList.contains(clickedClass)){
+    //     h1.classList.remove(clickedClass);
+    // } else{
+    //     h1.classList.add(clickedClass);
+    // }
 }
 
 h1.addEventListener("click", handleh1Click);
