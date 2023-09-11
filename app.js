5ae54bc08601b9ef3bc2b0627bdc87cdda10db2c
soyoung <soyseo99@naver.com>
2023-09-11 21:26:04 +0900

    4.1 - input value 유효성 확인

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 function loginBtnClick(){
-    console.log("hello ",loginInput.value)
+    const name = loginInput.value
+    
+        console.log("hello ",name)
+   
 }
 loginButton.addEventListener("click",loginBtnClick)
