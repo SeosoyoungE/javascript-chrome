bd975ffd19b8b5cc1f5454399267fb8cae329bba
soyoung <soyseo99@naver.com>
2023-09-12 00:54:40 +0900

    4.3 - event.preventDefault html기본동작을 막는 함수

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+const link = document.querySelector("a");
 function onLoginSubmit(event) {
     info.preventDefault();
     console.log(event);
 }
+//이벤트 리스너를 사용하면 object결과를 얻을 수 있다. 그 변수가 event
+function linkClick(event){
+    event.preventDefault();
+    console.log(event)
+}
 
 loginForm.addEventListener("submit", onLoginSubmit)
+link.addEventListener("click",linkClick);
