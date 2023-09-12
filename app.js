05cf843ca195ec43be9c587c421cf2ae85a79bdd
soyoung <soyseo99@naver.com>
2023-09-12 13:48:46 +0900

    4,4 - ``(백틱)으로 spring과 변수를 합치는 법

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME="hidden"
+
 function onLoginSubmit(event) {
-    info.preventDefault();
-    console.log(event);
-}
-//이벤트 리스너를 사용하면 object결과를 얻을 수 있다. 그 변수가 event
-function linkClick(event){
     event.preventDefault();
-    console.log(event)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const name=loginInput.value;
+    greeting.innerHTML=`Hello ${name}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
-link.addEventListener("click",linkClick);
