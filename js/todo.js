11a49203a9d220b0ab9de529547f3afae6ae9895
soyoung <soyseo99@naver.com>
2023-09-17 04:14:41 +0900

    7.2 - 투두리스트 삭제 구현

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,21 @@ const toDoFrom =document.querySelector("#todo-form");
 const toDoInput = toDoFrom.querySelector("input");
 const toDolist = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function showToDo(newTodo){
     const li = document.createElement("li");
     const span =document.createElement("span");
-    li.appendChild(span);
     span.innerHTML = newTodo;
+    const button= document.createElement("button");
+    button.innerHTML="X";
+    li.appendChild(span);
+    li.appendChild(button);
     toDolist.appendChild(li);
+    button.addEventListener("click",deleteToDo);
 }
 
 function toDoSubmit(event){
@@ -16,5 +25,6 @@ function toDoSubmit(event){
     toDoInput.value="";
     showToDo(newTodo);
 }
+
 
 toDoFrom.addEventListener("submit",toDoSubmit);
