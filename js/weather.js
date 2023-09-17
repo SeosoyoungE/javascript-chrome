f53a776e67a6aea83538b874d5c3827e6825bcaa
soyoung <soyseo99@naver.com>
2023-09-17 22:08:27 +0900

    8.1 - 날씨api를 이용하여 json을 받아와서 사용자의 위치와 날씨띄우기

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,14 @@
+const API_KEY="d48720e512f2cc336320189954a85820"
+
 function onGeoOk(position){
     const lat=position.coords.latitude;
-    const lng=position.coords.longitude;
-    console.log("you live in",lat,lng)
+    const lon=position.coords.longitude;
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}6&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data=>{
+        const weather=document.querySelector("#weather span:last-child");
+        const city=document.querySelector("#weather span:first-child");
+        city.innerHTML=data.name+" :";
+        weather.innerHTML=`${data.weather[0].main}/${data.main.temp}`});
 }
 function onGeoError(){
     alert("can't find you!!")
