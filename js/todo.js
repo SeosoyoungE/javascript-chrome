c67c286065d3514eb07bef4e28a5a40e482a43ca
소 영 <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-08 16:00:50 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -57,7 +57,7 @@ if(savedToDos){//저장된 todo불러오기
 //아무내용 없을때 hidden
 function toDoHidden(){
     if(!localStorage.getItem("todos")){
-        toDolist.classList.contains("hidden")
+        toDolist.classList.add("hidden")
     }else{
         toDolist.classList.remove("hidden")
     }
